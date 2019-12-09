d7f495558b5b8ca2a6d3bfd4e104126087cbee02
Arthur TRAN-TAO <arthurtran-tao@MacBook-Air-dArthur.local>
2019-12-09 15:00:49 +0100

    On avance du coup...

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,26 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
     // write your code here
-    System.out.println("Hello World !");
+        boolean choice = true; 
+        Scanner sc = new Scanner(System.in);
+        String entry; 
+        while(choice){
+            System.out.println("Bienvenue aventurier, dans le doux monde de la défaite");
+            entry = sc.nextLine();
+            switch(entry){
+                case "exit":
+                    choice = false;
+                    break;
+                case "help":  
+            }
+        }
+    }
+    public void helpCommand(){
+
     }
 }

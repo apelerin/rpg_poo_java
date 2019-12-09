4e54e278ae50d20ae8de2995f9eb37b5a518b4d2
Zatoster <Zatoster.mc@gmail.com>
2019-12-09 22:29:37 +0100

    Ajout de la classe Guerrier et de son Shield

---
+++ src/com/company/Warrior.java
@@ -0,0 +1,16 @@
+package com.company;
+
+protected class Warrior extends Character {
+    int shield;
+    //int reduceDmg;
+
+    public int getShield{
+        return this.shield;
+    } 
+    /*public int getReduceDmg{
+        if shield > damage{
+
+        }
+    }*/
+    
+}

ab80380411260b96c56c5b7678c2bc55f89cf2eb
Zatoster <Zatoster.mc@gmail.com>
2019-12-10 10:30:49 +0100

    Ajoit de la création de personnage

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.util.Scanner;
+import java.util.jar.Attributes.Name;
 
 public class Main {
 
@@ -9,7 +10,7 @@ public class Main {
         boolean choice = true; 
         Scanner sc = new Scanner(System.in);
         String entry;
-        System.out.println("Bienvenue aventurier, dans le doux monde de la défaite.");
+        System.out.println("Welcome newcomer... Into this sweet world of defeat...");
         while(choice) {
             System.out.println("Que voulez vous faire?");
             entry = sc.nextLine();
@@ -21,6 +22,9 @@ public class Main {
                 case "help":
                     helpCommand();
                     break;
+                case "new character":
+                    createCharacter();
+                    break;
                 default:
                     System.out.println("Invalid command.");
             }
@@ -30,5 +34,27 @@ public class Main {
     public static void helpCommand() {
         System.out.println("help : Show this help menu.");
         System.out.println("exit : Exit the game.");
+    }
+    public static void createCharacter(Character) {
+        Scanner sc = new Scanner(System.in);
+        String entry;
+        System.out.println("Type here, the stats of your character...");
+        entry = sc.nextLine();
+        System.out.println("What is the name of your character ?");
+        System.out.println(" ");
+        name = entry;
+        entry = sc.nextLine();
+        System.out.println("What is the strenght of your character ?");
+        System.out.println(" ");
+        damage = entry;
+        entry = sc.nextLine();
+        System.out.println("What is the vitality of your character ?");
+        System.out.println(" ");
+        hp = entry;
+        entry = sc.nextLine();
+        System.out.println("What is the speed of your character ?");
+        System.out.println(" ");
+        speed = entry;
+        entry = sc.nextLine();
     }
 }
